45b20a1c7316782ba0402ae6380b768cb42bfd71
Saarthak Rathee <saarthakrathee@Jeffs-iMac.local>
2019-02-20 15:53:05 -0500

    [thredded_create_app] Setup Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  devise_for :users,
+           skip: %i[sessions],
+           controllers: {
+             registrations: 'users/registrations',
+             sessions: 'users/sessions',
+           },
+           path_names: { sign_up: 'register' }
+devise_scope :user do
+  get 'sign-in', to: 'users/sessions#new', as: :new_user_session
+  post 'sign-in', to: 'users/sessions#create', as: :user_session
+  match 'sign-out', to: 'users/sessions#destroy', as: :destroy_user_session,
+                    via: Devise.mappings[:user].sign_out_via
+end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
